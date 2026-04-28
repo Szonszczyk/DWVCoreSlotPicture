0bb84e1fa4842664518926199fb750045223a014
Szonszczyk <Szonszczyk@users.noreply.github.com>
2026-04-28 18:54:31 +0200

    Add project files.

---
+++ Main.cs
@@ -0,0 +1,24 @@
+﻿using SPTarkov.DI.Annotations;
+using SPTarkov.Server.Core.DI;
+using System.Reflection;
+
+namespace DWVCoreSlotPicture;
+
+
+[Injectable(TypePriority = OnLoadOrder.PostDBModLoader + 2)]
+public class LoadSlotImagesUsingWTTCommonLib(
+    WTTServerCommonLib.WTTServerCommonLib? wttCommon
+) : IOnLoad
+{
+    public async Task OnLoad()
+    {
+        if (wttCommon == null)
+            return;
+
+        var assembly = Assembly.GetExecutingAssembly();
+
+        wttCommon.CustomSlotImageService.CreateSlotImages(assembly, Path.Join("db", "10_Slots"));
+
+        await Task.CompletedTask;
+    }
+}
